71d7b2c2a7af66b5de9a7bc74c565aea0ad9bf6a
mirceamaierean <mircea.maierean@yahoo.com>
2023-05-09 01:32:30 +0300

    run prettier

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { format, parseISO } from "date-fns";
 import { allPosts } from "contentlayer/generated";
 import { notFound } from "next/navigation";
-import { useMDXComponent } from 'next-contentlayer/hooks'
+import { useMDXComponent } from "next-contentlayer/hooks";
 
 import Link from "next/link";
 import { Circle } from "@/components/Circle";
@@ -19,28 +19,28 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
   if (!post) notFound();
 
-  const MDXComponent = useMDXComponent(post.body.code)
+  const MDXComponent = useMDXComponent(post.body.code);
 
   return (
     <div className="mt-32 sm:mt-4 mx-5 sm:mx-auto max-w-3xl bg-[#f7f7f7] dark:bg-[#2a2b33] rounded-xl border-2 border-[#f1f1f1] dark:border-[#2a2b33] ">
-       <Link href="/blog" className="flex ml-5">
-          <div className="scale-50">
-            <Circle
-              textContent="Back to all posts"
-              rotationAngle={90}
-              translateX={-10}
-            />
-          </div>
-        </Link>
-        <article className="mx-auto max-w-xl pb-8">
-          <div className="mb-8 text-center">
-            <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
-              {format(parseISO(post.date), "LLLL d, yyyy")}
-            </time>
-            <h1 className="text-3xl font-bold">{post.title}</h1>
-          </div>
-          <MDXComponent />
-        </article>
+      <Link href="/blog" className="flex ml-5">
+        <div className="scale-50">
+          <Circle
+            textContent="Back to all posts"
+            rotationAngle={90}
+            translateX={-10}
+          />
+        </div>
+      </Link>
+      <article className="mx-auto max-w-xl pb-8">
+        <div className="mb-8 text-center">
+          <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
+            {format(parseISO(post.date), "LLLL d, yyyy")}
+          </time>
+          <h1 className="text-3xl font-bold">{post.title}</h1>
+        </div>
+        <MDXComponent />
+      </article>
     </div>
   );
 };
