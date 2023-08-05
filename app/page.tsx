57942230a60321cd1bdacec411f85c7ca4c91db0
mirceamaierean <mircea.maierean@yahoo.com>
2023-08-06 01:45:29 +0300

    add toggle for dark mode: lint

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import Link from "next/link";
 import { Spotify } from "@/components";
 
 export default function Home() {
-  return <>
-    <Spotify />
-  </>;
+  return (
+    <>
+      <Spotify />
+    </>
+  );
 }
