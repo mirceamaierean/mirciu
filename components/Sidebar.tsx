aac8e9834b5d15685d7b768283f04110b9159218
mirceamaierean <mircea.maierean@yahoo.com>
2023-08-11 15:16:35 +0300

    prettier

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,7 +4,9 @@ const Sidebar = () => {
   return (
     <div className="sticky lg:top-[40px] dark:bg-stone-900 rounded-3xl border-gray-800 border m-2 lg:mt-10 max-h-[48rem] lg:w-60 flex flex-col p-4">
       <div className="mx-auto">
-        <h1 className="text-2xl dark:text-white font-matter font-bold">Mirciu</h1>
+        <h1 className="text-2xl dark:text-white font-matter font-bold">
+          Mirciu
+        </h1>
       </div>
     </div>
   );
