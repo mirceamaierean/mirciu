cf1df57ed9184d7baa01f43385c8474bad5628c9
mirceamaierean <mircea.maierean@yahoo.com>
2023-08-29 02:17:24 +0300

    added button for learn more

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import { Info } from "@/components";
 import Image from "next/image";
 import mirciu from "@/public/images/mirciu.png";
+import { Circle } from "@/components";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row justify-between mt-24">
         <div className="m-auto">
           <h1 className="text-5xl font-bold"> Hi there! I&apos;m Mirciu! </h1>
           <Info />
+          <Circle textContent="Learn more about me" translateY={10} />
         </div>
         <div className="mx-auto w-[25rem]">
           <Image src={mirciu} alt="mirciu" />
