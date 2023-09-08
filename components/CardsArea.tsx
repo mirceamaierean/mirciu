1acebbfdd32e289e07257840c12b49c1c34f3c22
mirceamaierean <mircea.maierean@yahoo.com>
2023-09-09 01:00:26 +0300

    add cards deck

--- components/CardsArea.tsx
+++ components/CardsArea.tsx
@@ -12,7 +12,7 @@ export const CardsArea = (props: CardsAreaProps) => {
   return (
     <div className="relative mx-auto">
       <motion.div
-        className=" dark:bg-white bg-neutral-500 rounded-3xl w-full h-screen opacity-5"
+        className=" dark:bg-white bg-neutral-500 rounded-xl w-full h-screen opacity-5"
         ref={constraintsRef}
       />
       <motion.div
@@ -23,14 +23,14 @@ export const CardsArea = (props: CardsAreaProps) => {
         {props.children}
       </motion.div>
       <motion.div
-        className="bg-gradient-to-br from-indigo-500 to-emerald-500 rounded-3xl h-96 w-60 absolute top-6 left-6"
+        className="bg-gradient-to-br from-indigo-500 to-emerald-500 rounded-xl h-96 w-60 absolute top-6 left-6"
         drag
         animate={{ x: 450, y: 0, opacity: 1 }}
         transition={{ duration: 1, delay: 0.5 }}
         dragConstraints={constraintsRef}
       />
       <motion.div
-        className="bg-gradient-to-l from-indigo-500 via-purple-500 to-pink-500 rounded-3xl absolute h-96 w-60 top-7 left-7"
+        className="bg-gradient-to-l from-indigo-500 via-purple-500 to-pink-500 rounded-xl absolute h-96 w-60 top-7 left-7"
         drag
         animate={{ x: 850, y: 0, opacity: 1 }}
         transition={{ duration: 1, delay: 0.5 }}
