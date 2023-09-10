7c3e48d3d508735bc6502f921276948dbfee3279
mirceamaierean <mircea.maierean@yahoo.com>
2023-09-11 01:03:06 +0300

    remove dot background

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import DotBackground from "@/components/DotBackground";
 
 export default function About() {
   return (
     <div className="mt-40">
-      <div className="min-h-screen bg-gray-200">
-        <DotBackground />
-        {/* Your other content */}
-      </div>
+
     </div>
   );
 }
