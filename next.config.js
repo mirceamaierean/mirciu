7a2365c06535fc011d7be6e78ab4d791e06e8419
Mircea Maierean <mircea.maierean@yahoo.com>
2024-01-25 17:03:44 +0200

    Fix xml2js version

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const { withContentlayer } = require("next-contentlayer");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: { appDir: true },
   images: {
     domains: ["i.scdn.co"], // Add any other allowed domains as well
     unoptimized: true,
